3830a742a7c68e3a8751fbf7307c7f83c7ab7493
daniellopesdarocha1 <daniellopesdarocha1@gmail.com>
2016-02-26 19:46:32 -0300

    Calculando IMC dinâmicamente

--- projeto/calcula-imc.js
+++ projeto/calcula-imc.js
@@ -1,26 +1,30 @@
-var peso = 100;
-var altura = 2.0;
 
-var tdPeso = document.getElementById("peso-2");
-var tdAltura = document.getElementById("altura-2");
+var trPacientes = document.getElementsByClassName("paciente"); // cria array de trs
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+for(var posicaoAtual = 0; posicaoAtual <= trPacientes.length - 1; posicaoAtual++){
 
-var paciente = {
-	peso : peso,
-	altura : altura
+	var pacienteTr = trPacientes[posicaoAtual];
+	var tdNome = pacienteTr.getElementsByClassName("info-nome")[0]; //[0] para pegar sempre a posição atual
+	var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
+	var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
+
+	var paciente = {
+		nome : tdNome.textContent,
+		peso : tdPeso.textContent,
+		altura: tdAltura.textContent
+	}
+
+
+	if (paciente.altura != 0){
+		var imc = paciente.peso / (paciente.altura * paciente.altura);
+		//var imc = paciente["peso"] / (paciente["altura"] * paciente["altura"]); //também pode-se usar esta notação
+
+		var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+		tdImc.textContent = imc;
+		console.log(imc);
+
+	}else{
+
+		console.log("Erro! altura não pode ser igual a zero.");
+	}
 }
-
-if (paciente.altura != 0){
-	var imc = paciente.peso / (paciente.altura * paciente.altura);
-	//var imc = paciente["peso"] / (paciente["altura"] * paciente["altura"]);
-
-	var tdImc = document.getElementById("imc-2");
-	tdImc.textContent = imc;
-
-}else{
-	console.log("Erro! altura não pode ser igual a zero.");
-}
-
-console.log(imc);
