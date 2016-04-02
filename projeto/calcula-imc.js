48653c831cac69ae1269e772503ab002f34c6ff9
Daniel Lopes da Rocha <daniellopesdarocha1@gmail.com>
2016-04-02 09:25:23 -0300

    Utilizando eventos com onclick e mouseover js

--- projeto/calcula-imc.js
+++ projeto/calcula-imc.js
@@ -1,36 +1,41 @@
+var botao = document.getElementById("calcula-imcs");
+//botao.onclick = calculaTodosImcs; // só aceita um evento para o botao
 
-var trPacientes = document.getElementsByClassName("paciente"); // cria array de trs
+botao.addEventListener("click", function (){
 
-percorreArray(trPacientes, function(pacienteTr){
+	var trPacientes = document.getElementsByClassName("paciente"); // cria array de trs
 
-	var tdNome = pacienteTr.getElementsByClassName("info-nome")[0]; //[0] para pegar sempre a posição atual
-	var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
-	var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
+	percorreArray(trPacientes, function(pacienteTr){
+		var tdNome = pacienteTr.getElementsByClassName("info-nome")[0]; //[0] para pegar sempre a posição atual
+		var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0];
+		var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
 
-	var pacienteAtual = {
-		nome : tdNome.textContent,
-		peso : tdPeso.textContent,
-		altura: tdAltura.textContent,
-		pegaImc : function(){
-				if (this.altura != 0){
+		var pacienteAtual = {
+			nome : tdNome.textContent,
+			peso : tdPeso.textContent,
+			altura: tdAltura.textContent,
+			pegaImc : function(){
+					if (this.altura != 0){
 
-					var imc = this.peso / (this.altura * this.altura);
-					return imc;
+						var imc = this.peso / (this.altura * this.altura);
+						return imc;
 
-				}else{
+					}else{
 
-					console.log("Erro! altura não pode ser igual a zero.");
-				}
-		}
-	};
+						console.log("Erro! altura não pode ser igual a zero.");
+
+					}
+			}
+		};
 
 
-	var imc = pacienteAtual.pegaImc();
+		var imc = pacienteAtual.pegaImc();
 
-	var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
-	tdImc.textContent = imc;
+		var tdImc = pacienteTr.getElementsByClassName("info-imc")[0];
+		tdImc.textContent = imc;
 
-	console.log(imc);
+		console.log(imc);
 
+	});
 
 });
--- projeto/percorre.js
+++ projeto/percorre.js
@@ -1,3 +1,5 @@
+var posicaoAtual = 0;
+
 function percorreArray(trPacientes, comportamento){
 	for(var posicaoAtual = 0; posicaoAtual <= trPacientes.length - 1; posicaoAtual++){
 		var pacienteTrAtual = trPacientes[posicaoAtual];
