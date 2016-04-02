e81068e92c4fba8c876991b1dde1358d59484319
Daniel Lopes da Rocha <daniellopesdarocha1@gmail.com>
2016-04-02 10:49:39 -0300

    Utilizando o seletor query para adicionar um novo paciente

---
+++ projeto/adiciona.js
@@ -0,0 +1,26 @@
+var botao = document.querySelector("#adicionar-paciente");
+
+botao.addEventListener("click", function(event){
+
+	event.preventDefault(); // impede comportamento padrão de submeter em formularios
+
+	var campoNome = document.querySelector("#campo-nome");
+	var campoPeso = document.querySelector("#campo-peso");
+	var campoAltura = document.querySelector("#campo-altura");
+
+	var pacienteNovo = 	"<tr class='paciente'>"+
+							"<td class='info-nome'>"+ campoNome.value+"</td>"+
+							"<td class='info-peso'>"+ campoPeso.value+"</td>"+
+							"<td class='info-altura'>"+ campoAltura.value+"</td>"+
+							"<td class='info-imc'></td>"+
+						"</tr>";
+
+	var tabela = document.querySelector("table"); //retorna o primeiro elemento
+	tabela.innerHTML = tabela.innerHTML + pacienteNovo;
+
+
+	campoNome.value = "";
+	campoPeso.value = "";
+	campoAltura.value = "";
+
+})
